2ea27a0c8903588c773404d3cb902b7131e5bf49
NgoPhuocLoi <87887247+NgoPhuocLoi@users.noreply.github.com>
2022-03-03 20:28:13 +0700

    Update client.js

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: 3t8pzdvf,
+  projectId: "3t8pzdvf",
   dataset: "production",
   apiVersion: "2022-02-28",
   useCdn: true,
-  token: sk1kmIk1YVXSpiDiacyb7dpQKklqKZTzoXDp1PbwHqF9rB1DJbqxyQxVYnrZh6eyIIMeHb0yswzhSiQOlLf9GhKO72JRegfKqKlmkflMPNNZ4EdZcyvbbWkxYSahKQ4s8Sk8qjXxTeUgovoHZr6YlkJO8GDUX05skOM79Ohw9SgOjZZXyDlP,
+  token: "sk1kmIk1YVXSpiDiacyb7dpQKklqKZTzoXDp1PbwHqF9rB1DJbqxyQxVYnrZh6eyIIMeHb0yswzhSiQOlLf9GhKO72JRegfKqKlmkflMPNNZ4EdZcyvbbWkxYSahKQ4s8Sk8qjXxTeUgovoHZr6YlkJO8GDUX05skOM79Ohw9SgOjZZXyDlP,
 });
 
 const builder = imageUrlBuilder(client);
